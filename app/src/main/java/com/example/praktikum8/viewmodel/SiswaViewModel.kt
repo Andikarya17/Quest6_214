bb07aefdf67de972af649ad73daf2a46a10c7eb2
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-20 13:48:24 +0700

    Menambahkan SiswaViewModel untuk mengelola state data siswa menggunakan StateFlow

---
+++ app/src/main/java/com/example/praktikum8/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,25 @@
+//SiswaViewModel.kt
+package com.example.praktikum8.viewmodel
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
+import com.example.praktikum8.model.Siswa
+import kotlinx.coroutines.flow.asStateFlow
+
+class SiswaViewModel : ViewModel() {
+
+    private val _statusUI = MutableStateFlow(Siswa())
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2]
+            )
+        }
+    }
+}
