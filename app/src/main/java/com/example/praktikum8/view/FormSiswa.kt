abb2d4cadbb0e06a5b3c11c57818c8efa8508481
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-20 13:34:38 +0700

    Menambahkan import yg ga kepanggil

--- app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
+++ app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
@@ -1,5 +1,18 @@
 package com.example.praktikum8.view
 
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.Alignment
+import com.example.praktikum8.R
+
+
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormSiswa(
