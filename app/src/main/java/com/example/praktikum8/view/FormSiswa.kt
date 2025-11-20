fdd529c9db332171c60de2170e1311ff6035316b
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-20 13:30:15 +0700

    Menambahkan struktur Scaffold dan TopAppBar pada FormSiswa

--- app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
+++ app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
@@ -10,3 +10,17 @@ fun FormSiswa(
     var txtNama by remember { mutableStateOf("") }
     var txtAlamat by remember { mutableStateOf("") }
     var txtGender by remember { mutableStateOf("") }
+
+    Scaffold(
+        modifier = Modifier,
+        topBar = {
+            TopAppBar(
+                title = { Text(text = stringResource(id = R.string.app_name), color = Color.White) },
+                colors = TopAppBarDefaults.mediumTopAppBarColors(
+                    containerColor = colorResource(id = R.color.purple_500)
+                )
+            )
+        }
+    )
+
+}
