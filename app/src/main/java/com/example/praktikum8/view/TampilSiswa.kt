b0a4c548a45e412b12e202c19c762323b089e16f
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-20 13:41:28 +0700

    Menambahkan list pasangan label dan nilai untuk menampilkan detail siswa

--- app/src/main/java/com/example/praktikum8/view/TampilSiswa.kt
+++ app/src/main/java/com/example/praktikum8/view/TampilSiswa.kt
@@ -6,4 +6,10 @@ fun TampilSiswa(
     statusUiSiswa: Siswa,
     onBackButtonClicked: () -> Unit
 ) {
+    val items = listOf(
+        Pair(stringResource(id = R.string.nama), statusUiSiswa.nama),
+        Pair(stringResource(id = R.string.gender), statusUiSiswa.gender),
+        Pair(stringResource(id = R.string.alamat), statusUiSiswa.alamat)
+    )
+
 }
