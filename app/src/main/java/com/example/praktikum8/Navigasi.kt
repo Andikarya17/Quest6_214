04f9a5e9f84d72b8b7229474700df5d598966517
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-20 13:55:21 +0700

    Menambahkan navigasi ke halaman Detail serta fungsi kembali ke Formulir

--- app/src/main/java/com/example/praktikum8/Navigasi.kt
+++ app/src/main/java/com/example/praktikum8/Navigasi.kt
@@ -1,5 +1,23 @@
 package com.example.praktikum8
 
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.praktikum8.model.DataJK
+import com.example.praktikum8.view.FormSiswa
+import com.example.praktikum8.view.TampilSiswa
+import com.example.praktikum8.viewmodel.SiswaViewModel
+import androidx.lifecycle.viewmodel.compose.viewModel
+
+
 enum class Navigasi {
     Formulir,
     Detail
@@ -36,6 +54,28 @@ fun SiswaApp(
                     }
                 )
             }
+            composable(route = Navigasi.Detail.name) {
 
-
+                TampilSiswa(
+                    statusUiSiswa = uiState.value,
+                    onBackButtonClicked = {
+                        cancelAndBackToFormulir(navController)
+                    }
+                )
+            }
         }
+
+    }
+
+    private fun cancelAndBackToFormulir(
+        navController: NavHostController
+    ) {
+        navController.popBackStack(
+            route = Navigasi.Formulir.name,
+            inclusive = false
+        )
+    }
+
+
+
+}
