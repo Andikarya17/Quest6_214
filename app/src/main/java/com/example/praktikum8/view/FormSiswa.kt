04183e014888fd501e11fbcda0255de2b85b1440
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-20 13:29:11 +0700

    Inisialisasi FormSiswa composable dengan variable dan parameters

--- app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
+++ app/src/main/java/com/example/praktikum8/view/FormSiswa.kt
@@ -1,1 +1,12 @@
 package com.example.praktikum8.view
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormSiswa(
+    pilihanJK: List<String>,
+    onSubmitButtonClicked: (MutableList<String>) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    var txtNama by remember { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
