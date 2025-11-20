f4503739df0e6abe1a2f4a99cd8667f34378123d
Andikarya17 <andikaryaputra17@gmail.com>
2025-11-20 13:53:32 +0700

    Menambahkan NavHost dan konfigurasi navigasi untuk halaman Formulir

--- app/src/main/java/com/example/praktikum8/Navigasi.kt
+++ app/src/main/java/com/example/praktikum8/Navigasi.kt
@@ -16,4 +16,26 @@ fun SiswaApp(
 
         val uiState = viewModel.statusUI.collectAsState()
 
-    }
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulir.name,
+            modifier = Modifier.padding(isiRuang)
+        ) {
+
+            composable(route = Navigasi.Formulir.name) {
+
+                val konteks = LocalContext.current
+
+                FormSiswa(
+                    pilihanJK = DataJK.JenisK.map { id ->
+                        konteks.resources.getString(id)
+                    },
+                    onSubmitButtonClicked = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+
+
+        }
